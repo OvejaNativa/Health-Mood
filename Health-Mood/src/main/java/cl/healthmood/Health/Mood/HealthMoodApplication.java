55cd3e29b76e60dec14c44b94eaa590233debccc
OvejaNativa <phoffmannh@gmail.comgit config -- global user.name OvejaNativagit config --global user.email phoffmannh@gmail.com>
2025-08-06 10:13:51 -0400

    SpringBoot

---
+++ Health-Mood/src/main/java/cl/healthmood/Health/Mood/HealthMoodApplication.java
@@ -0,0 +1,13 @@
+package cl.healthmood.Health.Mood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthMoodApplication.class, args);
+	}
+
+}
