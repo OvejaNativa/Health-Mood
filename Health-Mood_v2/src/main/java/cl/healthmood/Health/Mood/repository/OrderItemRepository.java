cccb16a6de592d7ebe0848ca0ce5539fe8f5fbcf
moredevj <defeds009@gmail.com>
2025-08-08 18:08:49 -0400

    Delete Health-Mood_v2 directory

--- Health-Mood_v2/src/main/java/cl/healthmood/Health/Mood/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.healthmood.Health.Mood.repository;
-
-import cl.healthmood.Health.Mood.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
-}
